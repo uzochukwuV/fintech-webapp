2ebbe646ddc216e909042752ded5ee1536b58f9e
uuzor <victor.u.ezealor@gmail.com>
2024-03-30 17:26:32 +0100

    feat: delete dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         <Provider>
         <ThemeProvider
         attribute="class"
-        defaultTheme="light"
+        defaultTheme="dark"
         enableSystem
         disableTransitionOnChange
         >
